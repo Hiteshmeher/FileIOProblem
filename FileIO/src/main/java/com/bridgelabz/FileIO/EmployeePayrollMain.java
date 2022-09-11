ff7db5aedc7e16ccb0d3f60ee397b9342bf031c8
Hiteshmeher <meher.hitesh4@gmail.com>
2022-09-11 23:40:14 +0530

    [Hitesh]: added code for Employee Payroll Service to read and write to a Console

---
+++ FileIO/src/main/java/com/bridgelabz/FileIO/EmployeePayrollMain.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.FileIO;
+
+import java.util.Scanner;
+
+public class EmployeePayrollMain {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        // creating object of EmployeePayrollService class
+        EmployeePayrollService emp = new EmployeePayrollService();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Employee Payroll System");
+        while (true) {
+            // taking user input and calling the methods
+            System.out.println("Please select \n1.Add Employee Data\n2.View Employee Data\n3.EXIT");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    emp.readEmployeeData();
+                    break;
+                case 2:
+                    emp.writeEmployeeData();
+                    break;
+                case 3:
+                    System.exit(0);
+                default:
+                    System.out.println("Please enter valid details");
+            }
+        }
+    }
+}
